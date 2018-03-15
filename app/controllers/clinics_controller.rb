5ba75643a872377c8b3aa0cfc1b732a9ca319a20
XinHui <xhleong@Leongs-MacBook-Pro.local>
2018-03-15 11:07:08 +0800

    Added new clinic models and create form for new and edit clinics

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,9 +1,36 @@
 class ClinicsController < ApplicationController
-  
+    
+    def create
+
+    @doctor = Doctor.find(params[:doctor_id])
+    @clinic = @doctor.clinic.create(clinic_params)
+
+    redirect_to doctor_clinics_path(@doctor,@clinic)
+   
+   end
+
+    def edit
+      @clinic = Clinic.find(params[:id])
+    end
+
+  def update
+    @clinic = Clinic.find(params[:id])
+      if @doctor.update(doctor_params)
+        redirect_to @doctor
+      else
+        render 'edit'
+      end
+  end
+
+
+  def index
+    @doctor = Doctor.find(params[:doctor_id])
+    @clinic = @doctor.clinics.all
+  end
 
   private
-  def doctor_params
-    params.require(:clinic).permit(:name, :address, :city, :postcode, :country, :phone, :email, operation_hour:{}, :doctor_id)
+  def clinic_params
+    params.require(:clinic).permit(:name, :address, :city, :postcode, :country, :phone, :email, :doctor_id, :state, operation_hour:{})
   end
 
 end

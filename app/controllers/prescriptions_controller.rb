273db200f86e5a7ccde7bb0251d28590f1fd07ff
nextacademy <leejiemin92@gmail.com>
2018-03-16 16:11:17 +0800

    prescription mvc and routes

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,7 +1,7 @@
 class PrescriptionsController < ApplicationController
   before_action :require_login, except: :create
   before_action :find_record_with_id, only: [:new, :create, :edit, :update, :show, :destory]
-  before_action :find_presciption_with_id, only: [:edit, :update, :show, :destroy]
+  before_action :find_prescription_with_id, only: [:edit, :update, :show, :destroy]
   
   def index
     # @records = current_user.records.all
@@ -28,7 +28,7 @@ class PrescriptionsController < ApplicationController
     @prescription = Prescription.new(prescription_params)
     @prescription.record_id = params[:record_id]
     if @prescription.save
-      render record_prescriptions_path(@record) 
+      redirect_to patient_path(@user) 
     else 
       redirect_back(fallback_location: new_record_prescription_path)
       flash[:notice] = (@reservation.errors.full_messages)
@@ -42,14 +42,23 @@ class PrescriptionsController < ApplicationController
   end
 
   def update
+    @prescription.update(prescription_params)
+    redirect_to patient_path(current_user), :flash => {:success=> "Your record has been updated."}
   end
 
-  def delete
+  def destroy
+    @prescription.destroy
+    if current_user.type == 'Patient'
+      redirect_to patient_path(current_user.id), :flash => {:success=> "Your record has been deleted."}
+    elsif current_user.type == 'Doctor'
+      redirect_to doctor_path(current_user.id), :flash => {:success=> "Your record has been deleted."} 
+    end
+
   end
 
   private
   def prescription_params
-    params.require(:prescription).permit(:drug_name, :dosage) 
+    params.require(:prescription).permit(:drug_name, :dosage, :pres_start_date, :pres_end_date, :meal, :morning, :noon, :night) 
   end
 
   def find_record_with_id
@@ -57,7 +66,7 @@ class PrescriptionsController < ApplicationController
   end
 
   def find_prescription_with_id
-    @prescription = Prescription.find(params[:prescription_id])
+    @prescription = Prescription.find(params[:id])
   end
 
 end

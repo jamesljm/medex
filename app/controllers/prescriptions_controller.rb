8ca319af12f07ce2e93ee3bdcb2c9a3b52d6138a
nextacademy <leejiemin92@gmail.com>
2018-03-16 19:16:10 +0800

    prescription edit delete add

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,6 +1,6 @@
 class PrescriptionsController < ApplicationController
   before_action :require_login, except: :create
-  before_action :find_record_with_id, only: [:new, :create, :edit, :update, :show, :destory]
+  before_action :find_record_with_id, only: [:index, :new, :create, :edit, :update, :show, :destory]
   before_action :find_prescription_with_id, only: [:edit, :update, :show, :destroy]
   
   def index
@@ -21,6 +21,9 @@ class PrescriptionsController < ApplicationController
 
   def new
     @prescription = Prescription.new
+    respond_to do |format|
+      format.js
+    end
   end
 
   def create

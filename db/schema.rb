7306c726924b5b0f59d7d1c15de5d751d0f72b31
nextacademy <leejiemin92@gmail.com>
2018-03-17 15:11:53 +0800

    bundled install from previous git merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_03_15_172555) do
-=======
 ActiveRecord::Schema.define(version: 2018_03_16_082342) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,18 +112,6 @@ ActiveRecord::Schema.define(version: 2018_03_16_082342) do
     t.datetime "updated_at", null: false
     t.index ["doctor_id"], name: "index_likes_on_doctor_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text "body"
-    t.bigint "conversation_id"
-    t.bigint "patient_id"
-    t.bigint "doctor_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["doctor_id"], name: "index_messages_on_doctor_id"
-    t.index ["patient_id"], name: "index_messages_on_patient_id"
   end
 
   create_table "patients", force: :cascade do |t|
